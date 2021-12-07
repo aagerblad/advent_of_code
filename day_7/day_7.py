cd4315ea6f831ef68dfdb479b7ebd903a43791d4
Andreas Agerblad <aagerblad@spotify.com>
2021-12-07 09:31:27 -0500

    Add day 7

---
+++ day_7/day_7.py
@@ -0,0 +1,22 @@
+from audioop import avg
+import re
+import numpy as np
+
+filename = "day_7/input.txt"
+crabs = np.loadtxt(filename, dtype="int", delimiter=",", max_rows=1)
+
+max_value = np.max(crabs)
+
+lowest_fuel = max_value * len(crabs)
+last_fuel = lowest_fuel
+for pos in range(max_value):
+	fuel = np.sum(np.abs(crabs-pos))
+	if fuel > last_fuel:
+		break
+	if fuel < lowest_fuel:
+		lowest_fuel = fuel
+	last_fuel = fuel
+	print(pos, fuel, lowest_fuel, last_fuel)
+
+	
+print(lowest_fuel)
---
+++ day_7/day_7_pt2.py
@@ -0,0 +1,24 @@
+from audioop import avg
+import re
+import numpy as np
+
+filename = "day_7/input.txt"
+crabs = np.loadtxt(filename, dtype="int", delimiter=",", max_rows=1)
+
+max_value = np.max(crabs)
+
+lowest_fuel = max_value * len(crabs) * max_value
+last_fuel = lowest_fuel
+for pos in range(max_value):
+	movement = np.abs(crabs-pos)
+
+	fuel = int(np.sum(((movement*movement)+movement)/2))
+	if fuel > last_fuel:
+		break
+	if fuel < lowest_fuel:
+		lowest_fuel = fuel
+	last_fuel = fuel
+	print(pos, fuel, lowest_fuel, last_fuel)
+
+	
+print(lowest_fuel)
