615da09adfb5f5d70707184e2d6e2678a9622223
Andreas Agerblad <aagerblad@spotify.com>
2022-11-14 19:12:02 -0500

    Finally solved day 4 pt 2

--- 2021/clojure/day_4/core.clj
+++ 2021/clojure/day_4/core.clj
@@ -5,7 +5,7 @@
 
 (def input_
   (str/split
-   (slurp "day_4/test_input.txt") #"\n\n"))
+   (slurp "day_4/input.txt") #"\n\n"))
 
 (def draws (str/split (first input_) #","))
 
@@ -16,6 +16,9 @@
 
 (def boards
   (map create_board (rest input_)))
+
+(defn print_boards [boards]
+  (dorun (map #(print % "\n") (util/transpose boards))))
 
 (defn mark_board [board val]
   (map #(replace {val "x"} %) board))
@@ -49,6 +52,9 @@
         [new_boards draw (.indexOf completed_boards true)]
         (draw_numbers new_boards (rest draws))))))
 
+
+(print_boards boards)
+
 (let [results (draw_numbers boards draws)]
   (*
    (edn/read-string (second results))
@@ -61,30 +67,45 @@
            (nth (first results) (last results))))))))
 
 (defn drop_boards [boards completed_boards]
-  (println "completed boards" completed_boards)
-  (if (some true? completed_boards)
-    (drop_boards
-     (util/drop-index boards (.indexOf completed_boards true))
-     (util/drop-index completed_boards (.indexOf completed_boards true)))
-    boards))
+  (let [idx (.indexOf completed_boards true)]
+    (println "completed_boards: " completed_boards)
+    (println "index: " idx)
+    (println ">= idx 0 " (>= idx 0))
+    (if (>= idx 0)
+      (let [remaining_boards (util/drop-nth idx boards)
+            remaining_completed (util/drop-nth idx completed_boards)]
+        (println remaining_completed)
+        (if (some true? remaining_completed)
+          (drop_boards remaining_boards remaining_completed)
+          remaining_boards))
+      boards)))
 
 (defn draw_numbers_last_board [boards draws]
-  (println boards)
+  ;; (if (> 3 (count boards))
+  ;;   (println (first draws) boards)
+  ;;   (println (first draws) (count boards)))
+  (println " ")
+  (println (first draws))
   (if (empty? draws)
-    [boards "" nil]
+    [boards ""]
     (let [draw (first draws)
           new_boards (mark_boards boards draw)
           completed_boards (test_complete new_boards)]
+      (dorun (print_boards new_boards))
+      (println "completed boards: " completed_boards)
+      (println (some true? completed_boards))
       (if (some true? completed_boards)
         (if (= 1 (count new_boards))
-          [new_boards draw (.indexOf completed_boards true)]
+          [new_boards draw]
           (draw_numbers_last_board
            (drop_boards new_boards completed_boards)
            (rest draws)))
         (draw_numbers_last_board new_boards (rest draws))))))
 
+(draw_numbers_last_board boards draws)
 
 (let [results (draw_numbers_last_board boards draws)]
+  (println (second results))
   (*
    (edn/read-string (second results))
    (apply
@@ -93,5 +114,7 @@
          (filter
           #(not= "x" %)
           (flatten
-           (nth (first results) (last results))))))))
+           (first (first results))))))))
+
+
 
--- 2021/clojure/util.clj
+++ 2021/clojure/util.clj
@@ -3,5 +3,5 @@
 (defn transpose [m]
   (apply mapv vector m))
 
-(defn drop-index [col idx]
-  (filter identity (map-indexed #(if (not= %1 idx) %2) col)))
+(defn drop-nth [n coll]
+  (keep-indexed #(if (not= %1 n) %2) coll))
