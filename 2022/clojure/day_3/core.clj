e3a0c31c24f339bcc060c8779333ee670aee757b
Andreas Agerblad <aagerblad@spotify.com>
2022-12-03 12:29:34 -0500

    Code cleanup

--- 2022/clojure/day_3/core.clj
+++ 2022/clojure/day_3/core.clj
@@ -1,13 +1,13 @@
 (ns core
   (:require [clojure.string :as str]))
 
-(defn common-char
-  [[s1 & s2]] (reduce
-               (fn [_ c]
-                 (when (every? #(some #{c} %) s2)
-                   (reduced c)))
-               nil
-               s1))
+(defn common-char [[s1 & s2]]
+  (reduce
+   (fn [_ c]
+     (when (every? #(some #{c} %) s2)
+       (reduced c)))
+   nil
+   s1))
 
 (defn convert-char-to-prio [c]
   (let [i (int c)] (if (< i 91) (- i 38) (- i 96))))
@@ -19,12 +19,10 @@
        (map char-array)))
 
 ;; Part 1
-(def input-part-1
-  (map #(split-at (/ (count %) 2) %) input))
+(def input-part-1 (map #(split-at (/ (count %) 2) %) input))
 
-(apply +
-       (map convert-char-to-prio
-            (map common-char input-part-1)))
+(apply + (map convert-char-to-prio
+              (map common-char input-part-1)))
 
 ;; Part 2
 (def input-part-2 (partition 3 input))
