3085f176d2ce808011f1e3c8649b957b3f1c0fbb
Andreas Agerblad <aagerblad@spotify.com>
2022-12-07 08:25:52 -0500

    Clean up day 6

--- 2022/clojure/day_6/core.clj
+++ 2022/clojure/day_6/core.clj
@@ -1,22 +1,18 @@
-(ns core
-  (:require [clojure.string :as str]
-            [clojure.edn :as edn]))
+(ns core)
 
 (def input
   (->> "day_6/input.txt"
        slurp
        drop-last))
 
+(defn find-distinct [i t]
+  (let [i (inc i)]
+    (if (= (count t) (count (distinct t)))
+      (reduced i)
+      i)))
+
 ;; Part 1
-(reduce (fn [i t]
-          (let [i (inc i)]
-            (if (= (count t) (count (distinct t)))
-              (reduced i)
-              i))) 3 (partition 4 1 input))
+(reduce find-distinct 3 (partition 4 1 input))
 
 ;; Part 2
-(reduce (fn [i t]
-          (let [i (inc i)]
-            (if (= (count t) (count (distinct t)))
-              (reduced i)
-              i))) 13 (partition 14 1 input))
+(reduce find-distinct 13 (partition 14 1 input))
