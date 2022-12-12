6432849dfd81e24c15826107a26c1ebb1aabea87
Andreas Agerblad <aagerblad@spotify.com>
2022-12-12 17:02:13 -0500

    Clean day 9

--- 2022/clojure/day_9/core.clj
+++ 2022/clojure/day_9/core.clj
@@ -3,21 +3,6 @@
             [clojure.string :as str]
             [util :as util]))
 
-(def input
-  (->> "day_9/input.txt"
-       slurp
-       str/split-lines
-       (map #(str/split % #"\s"))
-       (map (fn [[a b]] [(first (char-array a)) (edn/read-string b)]))
-      ;;  (map #(vec (map edn/read-string %)))
-       (#(reduce
-          (fn [instruction-list [dir length]]
-            (concat instruction-list (repeat length dir)))
-          []
-          %))))
-
-
-
 (defn adjust-tail [[hx hy] [tx ty]]
   (let [dx (- hx tx)
         dy (- hy ty)
@@ -36,8 +21,6 @@
    \R [1 0]
    \L [-1 0]})
 
-
-
 (defn adjust-head [[head & tail] dir]
   (let [new-head (vec (map + head (directions dir)))]
     (reduce
@@ -46,9 +29,20 @@
      [new-head]
      tail)))
 
+(def input
+  (->> "day_9/input.txt"
+       slurp
+       str/split-lines
+       (map #(str/split % #"\s"))
+       (map (fn [[a b]] [(first (char-array a)) (edn/read-string b)]))
+      ;;  (map #(vec (map edn/read-string %)))
+       (#(reduce
+          (fn [instruction-list [dir length]]
+            (concat instruction-list (repeat length dir)))
+          []
+          %))))
 ;; Part 1
 (def positions [[0 0] [0 0]])
-
 
 (count (second
         (reduce
@@ -57,8 +51,6 @@
              [new-pos (conj hist (last new-pos))]))
          [positions #{}]
          input)))
-
-
 
 ;; Part 2
 (def long-positions (vec (repeat 10 [0 0])))
